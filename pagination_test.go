c1a6000c7e20b5a2a730496cdc6797d441f64036
jackson <ramadhansalmanalfarisi8@gmail.com>
2023-03-20 15:24:16 +0700

    fix unit test

--- pagination_test.go
+++ pagination_test.go
@@ -1,13 +1,15 @@
 package go_pagination
 
 import (
+	"encoding/json"
 	"testing"
 )
 
 func TestCreatePagination(testing *testing.T) {
 	req_pagination := GetPagination(1, 10, 20)
 	pagination, err := req_pagination.CreatePagination()
-	testing.Log(pagination)
+	json,_ := json.Marshal(pagination)
+	testing.Log(string(json))
 	if err != nil {
 		testing.Errorf("error pagination test")
 	}
